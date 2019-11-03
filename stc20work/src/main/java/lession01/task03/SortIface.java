d35d80df348fd8f45a62443385f437312c80056d
Sergey Molodtsov <Molodtsov.S.Dm@sberbank.ru>
2019-11-03 21:25:45 +0500

    first commit

---
+++ stc20work/src/main/java/lession01/task03/SortIface.java
@@ -0,0 +1,12 @@
+package lession01.task03;
+
+/**
+ * Sorting interface
+ */
+public interface SortIface {
+
+    void firstMethodSort(Person[] array,int arraySize);
+
+    void secondMethodSort(Person[] array,int arraySize);
+
+}
