d188d6f154305803422bc8c223b159678619ea80
Sergey Molodtsov <Molodtsov.S.Dm@sberbank.ru>
2019-11-09 12:55:06 +0500

    fix home work 2

--- stc20work/src/main/java/lession02/task02/Task02.java
+++ stc20work/src/main/java/lession02/task02/Task02.java
@@ -13,11 +13,12 @@ public class Task02 {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Task 02");
-        System.out.println("input N (generate random numbers ) = ");
-        Scanner sc = new Scanner(System.in);
-        String N = sc.nextLine();
+//        System.out.println("input N (generate random numbers ) = ");
+//        Scanner sc = new Scanner(System.in);
+//        String n = sc.nextLine();
+          String n = "55";
 
-        randomMagic(Integer.valueOf(N));
+        randomMagic(Integer.valueOf(n));
     }
 
     /**
@@ -28,16 +29,24 @@ public class Task02 {
     private static void randomMagic(int length) throws Exception {
         Integer startArray[] = new Integer[length];
         Double sqrArray[]= new Double[length];
+        int mathX2;
 
         Random rnd = new Random();
         for( int i=0; i< length; i++){
             startArray[i] = rnd.nextInt(1000);
-            System.out.println("elements - "+startArray[i]);
+            //System.out.println("from -  "+startArray[i]);
             if (startArray[i] < 0) {
                 throw new Exception("в массиве есть отрицательные значения !");
             } else {
                 sqrArray[i] =  squareRoot(startArray[i]);
-                System.out.println(" sqrElements - "+sqrArray[i]);
+                mathX2 = mathPowX2(sqrArray[i]);
+                //System.out.println("--2 sqrT = "+sqrArray[i]);
+                //System.out.println("---3 x2 = "+mathX2);
+
+                if (startArray[i].equals(mathX2)) {
+                    System.out.println("elements result = " + mathX2);
+
+                }
             }
 
         }
@@ -51,5 +60,16 @@ public class Task02 {
      */
     private static double squareRoot(int squareElements) {
       return Math.sqrt(squareElements);
+    }
+
+    /**
+     * Math.pow (int) only main part
+     * @param sqrItem
+     * @return Pow result
+     */
+    private static int mathPowX2(double sqrItem) {
+        int resultMainPartX2 = 0;
+        resultMainPartX2 = (int) sqrItem;
+        return (int) Math.pow(resultMainPartX2, 2);
     }
 }
