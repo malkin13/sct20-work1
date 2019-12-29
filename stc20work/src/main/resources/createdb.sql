3bbaad874d2fa73237a267c07ee34089d19f2b34
Sergey Molodtsov <Molodtsov.S.Dm@sberbank.ru>
2019-12-29 21:31:25 +0500

    12 l

--- stc20work/src/main/resources/createdb.sql
+++ stc20work/src/main/resources/createdb.sql
@@ -26,3 +26,14 @@ create table user_role(
     user_id serial references "user"(id),
     role_id serial references role(id)
 );
+
+create table "user"
+(
+    id          serial PRIMARY KEY,
+    name        varchar(100) NOT NULL,
+    birthday    date         NOT NULL,
+    login_ID    serial       NOT NULL,
+    city        VARCHAR(100),
+    email       VARCHAR(100),
+    description VARCHAR(255)
+);
